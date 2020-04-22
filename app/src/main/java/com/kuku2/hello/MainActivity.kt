f74a2e484d2b9c2cc3445037af6e635518acec71
Xylitols <kuku20000@outlook.com>
2020-04-22 11:27:46 +0900

    Initial commit

---
+++ app/src/main/java/com/kuku2/hello/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kuku2.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // MainActivity가 최초 실행될 때 실행된다
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Activity 의 UI 를 R.layout.activity_main 으로 지정한다.
+        setContentView(R.layout.activity_main)
+    }
+}
