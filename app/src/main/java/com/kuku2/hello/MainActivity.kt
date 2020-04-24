3ba4a3d2a45d0ace281d7b2dd4f42e3e8023f10e
Xylitols <64079134+Xylitols@users.noreply.github.com>
2020-04-24 16:09:58 +0900

    87p

--- app/src/main/java/com/kuku2/hello/MainActivity.kt
+++ app/src/main/java/com/kuku2/hello/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {  // MainActivity가 최초 실행될 
         super.onCreate(savedInstanceState)  // Activity 의 UI 를 R.layout.activity_main 으로 지정한다.
         setContentView(R.layout.activity_main)
         Toast.makeText(getApplicationContext(),"프로그래밍을 시작해보자!",Toast.LENGTH_LONG).show() // "프로그래밍을 시작해보자!" 메세지를 잠시 보여준다
+
+
     }
 }
