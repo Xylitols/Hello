5fe3795c7a5a13f5f8421416d031b5fa630ed4e2
Xylitols <64079134+Xylitols@users.noreply.github.com>
2020-04-24 15:37:17 +0900

    85p

--- app/src/main/java/com/kuku2/hello/MainActivity.kt
+++ app/src/main/java/com/kuku2/hello/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.kuku2.hello
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    // MainActivity가 최초 실행될 때 실행된다
+class MainActivity : AppCompatActivity() {  // MainActivity가 최초 실행될 때 실행된다
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //Activity 의 UI 를 R.layout.activity_main 으로 지정한다.
-        // git test1
-
+        super.onCreate(savedInstanceState)  // Activity 의 UI 를 R.layout.activity_main 으로 지정한다.
         setContentView(R.layout.activity_main)
+        Toast.makeText(getApplicationContext(),"프로그래밍을 시작해보자!",Toast.LENGTH_LONG).show() // "프로그래밍을 시작해보자!" 메세지를 잠시 보여준다
     }
 }
